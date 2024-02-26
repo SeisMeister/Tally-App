8a155e92ce2fddb15483cde3403168d854104434
Cesar Fernandez <CesarFernandez2@live.com>
2024-02-26 09:14:31 -0700

    Initial Commit

---
+++ Tally App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally App
+//
+//  Created by Cesar Fernandez on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
